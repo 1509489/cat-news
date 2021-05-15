42e2c85c0e5748b8cde1a7473bd9c09b7bff6591
Justice <popystone@gmail.com>
2021-05-15 12:01:48 +0100

    Update NetworkService.kt

--- app/src/main/java/com/sky/catnews/network/NetworkService.kt
+++ app/src/main/java/com/sky/catnews/network/NetworkService.kt
@@ -4,6 +4,7 @@ import com.sky.catnews.models.News
 import com.sky.catnews.models.NewsStory
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface NetworkService {
 
@@ -11,5 +12,5 @@ interface NetworkService {
     suspend fun getNews(): Response<News>
 
     @GET("/story/{id}")
-    suspend fun getStory(id: String): Response<NewsStory>
+    suspend fun getStory(@Path("id") id: String): Response<NewsStory>
 }
